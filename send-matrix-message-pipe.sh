a1ca30a47bc1ea8fa10973567506e82eadee5922
Dusty Mabe <dusty@dustymabe.com>
2025-07-29 23:59:12 -0400

    support for piped HTML data
    This allows for piped data to be HTML and passed through to the
    matrix message rather than adding the `<code>` formatting as was
    default before.
    
    I needed this because I wanted to send hyperlinks and thus allowing
    the sending program to output HTML was the easiest thing to do here.

--- send-matrix-message-pipe.sh
+++ send-matrix-message-pipe.sh
@@ -8,8 +8,8 @@
 # 
 # It takes stdin and formats it into JSON using jq, then passes that to
 # the matrix API for sending a message [2]. It prepends the message with
-# $HOSTNAME and then formats the piped data as <code>, which is usually
-# what you want. 
+# $HOSTNAME and then will pass on HTML directly (if HTML) OR format the
+# piped data as <code> if not HTML.
 # 
 # The `timeout` command used here is to workaround a systemd issue [1]
 # because otherwise we'd wait forever since no EOF is ever sent. While
@@ -24,12 +24,23 @@ set -eu -o pipefail
 UNICODE_MATRIX_ROOM="%21${MATRIX_ROOM:1}"
 ENDPOINT="${MATRIX_SERVER}/_matrix/client/r0/rooms/${UNICODE_MATRIX_ROOM}/send/m.room.message?access_token=${MATRIX_ACCESS_TOKEN}"
 
-timeout 1s cat                                     | \
-    (echo "$HOSTNAME<code>"; cat ; echo '</code>') | \
-    jq --null-input --raw-input --slurp              \
-        '{ msgtype: "m.text",
-           format: "org.matrix.custom.html",
-           body: "see formatted_body",
-           formatted_body: inputs }'               | \
-    curl --json @- $ENDPOINT
+format_stdin() {
+    echo -n "<b>$HOSTNAME</b><br>"
+    # Read first character from stdin and if it is `<` assume
+    # what we are being provided is HTML. If not, then assume
+    # plain text and wrap in <code> block.
+    read -n1 firstchar
+    if [ "$firstchar" != '<' ]; then
+        echo -n "<code>${firstchar}"; cat ; echo -n '</code>'
+    else
+        echo -n "${firstchar}"; cat
+    fi
+}
+
+timeout 1s cat | format_stdin | jq --null-input --raw-input --slurp '
+    { msgtype: "m.text",
+      format: "org.matrix.custom.html",
+      body: "see formatted_body",
+      formatted_body: inputs }
+    ' | curl --json @- $ENDPOINT
 echo
